a33cf9eca933225bb10fa391c80d703515953c57
罗夕 <332810362@qq.com>
2017-06-28 10:21:39 +0800

    finish

--- ProcessManagement/config/waterline.js
+++ ProcessManagement/config/waterline.js
@@ -6,31 +6,37 @@ var mongoAdapter = require('sails-mongo');
 // models
 var User = require('../models/User');
 var Process = require('../models/Process');
+var Token = require('../models/Token');
+var Status = require('../models/Status');
+var Student = require('../models/Student');
 
 var orm = new Waterline();
 var wlconfig = {
     adapters: {
-        default: mongoAdapter,
+        default: mysqlAdapter,
         mysql: mysqlAdapter,
         mongo: mongoAdapter
     },
     connections: {
         'mysql': {
             adapter: 'mysql',
-            host: '60.205.220.42',
+            host: 'your host',
             user: 'root',
-            password: '****',
+            password: '******',
             chartset:'utf8',
             database:'db_node'
         },
         'mongo': {
             adapter: 'mongo',
-            url: 'mongodb://60.205.220.42/xblog'
+            url: 'mongodb://your host/db_name'
         }
     }
 };
 orm.loadCollection(User);
 orm.loadCollection(Process);
+orm.loadCollection(Token);
+orm.loadCollection(Status);
+orm.loadCollection(Student);
 
 exports.orm = orm;
 exports.config = wlconfig;
